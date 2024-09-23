27dcc519e36bbfd5d557e8a2f9086ab00608236d
Hanna Kunitskaya <hanna_kunitskaya@gmail.com>
2024-09-23 17:39:53 +0200

    change page 404 css

--- frontend/src/pages/Page404.jsx
+++ frontend/src/pages/Page404.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 const Page404 = () => {
   return (
-    <>
+    <div className='page404'>
         <h1>404 - Page Not Found</h1>
         <p>Sorry, the page you are looking for does not exist.</p>
         
         <Link to='/' className='changePageBtn'>Return to login page</Link>
-    </>
+    </div>
   )
 }
 
